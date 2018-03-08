fe79980087d1908127835e91ad7187fff9382712
patcurry <currypat1985@gmail.com>
2018-03-08 10:39:48 +0100

    made a couple tests and a .travis.yml file

---
+++ main/test_views.py
@@ -0,0 +1,25 @@
+from django.http import HttpRequest
+from django.test import TestCase
+from django.urls import resolve
+
+from main.views import index
+
+
+class SmokeTest(TestCase):
+
+    def test_bad_maths(self):
+        self.assertEqual(1 + 1, 2)
+
+
+class IndexViewTests(TestCase):
+    
+    def test_index_view_resolves(self):
+        found = resolve('/')
+        self.assertEqual(found.func, index)
+
+    def test_index_view_has_correct_html(self):
+        request = HttpRequest()
+        response = index(request)
+        html = response.content.decode('utf8')
+        self.assertIn('Hello, world!', html)
+
