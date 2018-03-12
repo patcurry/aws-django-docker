6ae14b12fe590e11c2233e36508eed66ea94e03f
patcurry <currypat1985@gmail.com>
2018-03-12 15:25:26 +0100

    making sure there is a template for todo-list

--- todos/test_views.py
+++ todos/test_views.py
@@ -9,3 +9,9 @@ class TodoListViewTest(TestCase):
     def test_todo_list_resolves(self):
         found = resolve('/todo-list')
         self.assertEqual(found.func, todo_list)
+
+    def test_todo_list_view_has_todo_list_in_html(self):
+        request = HttpRequest()
+        response = todo_list(request)
+        html = response.content.decode('utf8')
+        self.assertIn('Todo List', html)
