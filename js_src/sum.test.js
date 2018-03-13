b84c6fa13d7f2ff6a9ba7e5e12597fd222f59c40
patcurry <currypat1985@gmail.com>
2018-03-13 11:39:22 +0100

    moving files around and setting up .travis.yml for node tests

--- js_src/sum.test.js
+++ js_src/sum.test.js
@@ -1,5 +1,6 @@
 // index.test.js
-const sum = require('./index').sum;
+//const sum = require('./sum').sum;
+import { sum } from './sum';
 
 test('adds 1 and 11 to get 12', () => {
     expect(sum(1, 11)).toBe(12);
