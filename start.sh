a79916acef095f6847425e354b2fc7cc061fcbe8
patcurry <currypat1985@gmail.com>
2018-03-16 14:46:16 +0100

    removed stuff that docker needed and removed a templates folder from the main directory

--- start.sh
+++ start.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# Database Migrations
-#echo Make Migrations.
-#exec python manage.py makemigrations
-#
-#echo Migrate.
-#exec python manage.py migrate
-
-#exec python manage.py runserver 8000
-
 # Start Gunicorn processes
 echo Starting Gunicorn.
 exec gunicorn main.wsgi:application \
